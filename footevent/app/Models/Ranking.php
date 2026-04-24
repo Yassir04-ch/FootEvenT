13426bf2975f7204f3a746e84f08601d3a105e6c
yassir <yassirch245@gmail.com>
2026-04-24 23:43:23 +0200

    ajouter creation des ranking dans gameService avec logic pour virifie l'equipe qui win match et ajouter method updatePosition pour modifeir position de equipe dans tournoi et method updateranking

--- footevent/app/Models/Ranking.php
+++ footevent/app/Models/Ranking.php
@@ -9,17 +9,12 @@ class Ranking extends Model
     protected $fillable = ['points','position','matchWin','matchlos','goals_scored','equipe_id','tournoi_id',
     ];
 
-    /**
-     * Relation avec l'équipe
-     */
+
     public function equipe()
     {
         return $this->belongsTo(Equipe::class);
     }
 
-    /**
-     * Relation avec le tournoi
-     */
     public function tournoi()
     {
         return $this->belongsTo(Tournoi::class);
--- footevent/app/Service/ResultatService.php
+++ footevent/app/Service/ResultatService.php
@@ -34,40 +34,119 @@ class ResultatService{
         return $newNiveau;
     }
 
-    public function create(array $data,Game $game,$id_equipe1,$id_equipe2){
-
+    public function create(array $data, Game $game, $id_equipe1, $id_equipe2)
+    {
         $equipe1 = Equipe::find($id_equipe1);
         $equipe2 = Equipe::find($id_equipe2);
-        $equipe1pivot = $equipe1->tournois()->where('tournoi_id', $game->tournoi_id)->first();
-        $equipe2pivot = $equipe2->tournois()->where('tournoi_id', $game->tournoi_id)->first();
-        if($data['scoreEq1'] > $data['scoreEq2']){
-         $niveau = $equipe1pivot->pivot->niveau;
-         $newNiveau = $this->updateNiveau($niveau);
-          $equipe1->tournois()->updateExistingPivot($game->tournoi->id, ['niveau' => $newNiveau]);
-          $equipe2->tournois()->updateExistingPivot($game->tournoi->id, ['statut' => 'eliminate']);
-        }
-        else{
-         if($data['penaltyE1'] > $data['penaltyE2']){
+
+        $tournoi = $game->tournoi;
+        $tournoi_id = $tournoi->id;
+
+        $equipe1pivot = $equipe1->tournois()->where('tournoi_id', $tournoi_id)->first();
+        $equipe2pivot = $equipe2->tournois()->where('tournoi_id', $tournoi_id)->first();
+
+        $score1 = $data['scoreEq1'];
+        $score2 = $data['scoreEq2'];
+
+        if ($score1 > $score2) {
+
             $niveau = $equipe1pivot->pivot->niveau;
             $newNiveau = $this->updateNiveau($niveau);
-            $equipe1->tournois()->updateExistingPivot($game->tournoi->id, ['niveau' => $newNiveau]);
-            $equipe2->tournois()->updateExistingPivot($game->tournoi->id, ['statut' => 'eliminate']);
+
+            $equipe1->tournois()->updateExistingPivot($tournoi_id, ['niveau' => $newNiveau]);
+
+            $equipe2->tournois()->updateExistingPivot($tournoi_id, ['statut' => 'eliminate']);
+            $this->updateRanking($equipe1->id, $tournoi_id, $score1,3);
+            $this->updateRanking($equipe2->id, $tournoi_id, $score2,0);
+        }
+        elseif ($score2 > $score1) {
+
+            $niveau = $equipe2pivot->pivot->niveau;
+            $newNiveau = $this->updateNiveau($niveau);
+
+            $equipe2->tournois()->updateExistingPivot($tournoi_id, ['niveau' => $newNiveau]);
+
+            $equipe1->tournois()->updateExistingPivot($tournoi_id, ['statut' => 'eliminate']);
+
+            $this->updateRanking($equipe1->id, $tournoi_id, $score1,0);
+            $this->updateRanking($equipe2->id, $tournoi_id, $score2,3);
+        }
+        else {
+
+            $pen1 = $data['penaltyE1'];
+            $pen2 = $data['penaltyE2'];
+
+            if ($pen1 == $pen2) {
+                return ['success' => false, "message" => "Les penalties ne peuvent pas étre égaux"];
             }
-         else if($data['penaltyE1'] == $data['penaltyE2']){
-             return ['success'=>false , "message"=>"Les penalties ne peuvent pas être égaux"];
-         }
-         else{
+
+            if ($pen1 > $pen2) {
+
+                $niveau = $equipe1pivot->pivot->niveau;
+                $newNiveau = $this->updateNiveau($niveau);
+
+                $equipe1->tournois()->updateExistingPivot($tournoi_id, ['niveau' => $newNiveau]);
+
+                $equipe2->tournois()->updateExistingPivot($tournoi_id, ['statut' => 'eliminate']);
+
+                $this->updateRanking($equipe1->id, $tournoi_id, $score1,3);
+                $this->updateRanking($equipe2->id, $tournoi_id, $score2,0);
+
+            } else {
+
                 $niveau = $equipe2pivot->pivot->niveau;
                 $newNiveau = $this->updateNiveau($niveau);
-                $equipe2->tournois()->updateExistingPivot($game->tournoi->id, ['niveau' => $newNiveau]);
-                $equipe1->tournois()->updateExistingPivot($game->tournoi->id, ['statut' => 'eliminate']);
-             }
 
+                $equipe2->tournois()->updateExistingPivot($tournoi_id, ['niveau' => $newNiveau]);
+
+                $equipe1->tournois()->updateExistingPivot($tournoi_id, ['statut' => 'eliminate']);
+
+                $this->updateRanking($equipe1->id, $tournoi_id, $score1,0);
+                $this->updateRanking($equipe2->id, $tournoi_id, $score2,3);
+            }
         }
+        $this->updatePositions($tournoi_id);
         $this->repository->create($data);
-        $game->update(['statut'=>'termine']);
-        return ['success'=>true , "message"=>"Résultat ajouter"];
+        $game->update(['statut' => 'termine']);
 
+        dd($game);
+
+        return ['success' => true, "message" => "Résultat ajouté"];
+    }
+
+
+    public function updateRanking($equipe_id,$tournoi_id,$gols,$points){
+        $ranking = Ranking::where('equipe_id',$equipe_id)->where('tournoi_id',$tournoi_id)->first();
+        if(!$ranking){
+            Ranking::create([
+                "equipe_id"=>$equipe_id,
+                "tournoi_id"=>$tournoi_id
+            ]);
+        }
+        else{
+        if ($points == 3) {
+            $ranking->matchWin += 1;
+         } elseif ($points == 0) {
+            $ranking->matchlos += 1;
+         }
+          $goals_scored = $ranking->goals_scored + $gols;
+          $points = $ranking->points + $points;
+            $ranking->update([
+                'points'=>$points,
+                'matchWin'=>$matchWin,
+                'matchlos'=>$matchlos,
+                'goals_scored'=>$goals_scored
+            ]);
+        }
+    }
+
+    public function updatePositions($tournoi_id)
+    {
+        $rankings = Ranking::where('tournoi_id', $tournoi_id)->orderByDesc('points')->orderByDesc('goals_scored')->get();
+        foreach ($rankings as $index => $ranking) {
+            $position = $index + 1;
+            $ranking->update(['position'=>$position]);
+        }
     }
 
 }
